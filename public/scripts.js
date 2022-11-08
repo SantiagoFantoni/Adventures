da9387a8255421b76ab6a2ee41de26aa103053f1
Santiago Fantoni <fantonisantiago@gmail.com>
2022-11-08 19:11:22 -0300

    Key movement array

--- public/scripts.js
+++ public/scripts.js
@@ -2,15 +2,23 @@ let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
 const image = new Image();
 image.src = "/public/images/char.png";
-const loop = true;
+
+const keys = [];
 
 const player = new Entidad("Pedro", 10, 10, 10, image, 40, 40);
 const enemigo = new Entidad("Goblin", 8, 4, 4);
 
 function gameLoop() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	player.posx += 3;
 	player.Dibujar(ctx);
 	window.requestAnimationFrame(gameLoop);
 }
 gameLoop();
+
+window.addEventListener("keydown", (e) => {
+	keys.push(e.key);
+	console.log(keys[0]);
+});
+window.addEventListener("keyup", () => {
+	keys.shift();
+});
